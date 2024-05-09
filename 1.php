58c2dbeb6528ef7bd9470ff37bd064b454300327
Juan Martínez Alonso <juanmaal1986@gmail.com>
2024-05-09 12:43:49 +0200

    archivo 1.php igual que 0.php pero usando POO en lugar de programción estructurada

--- 1.php
+++ 1.php
@@ -5,4 +5,25 @@ class Usuario{
     public $nombre;
 
     //métodos (Funciones)
+    public function saluda(){
+        return $this->nombre.' dice hola';
+    }
 }
+
+//Instanciación de un objeto usuario de la clase Usuario
+$usuario1 = new Usuario();
+$usuario1->nombre='Juan';
+echo $usuario1->nombre;
+echo '<br>';
+echo $usuario1->saluda();
+
+echo '<br>';
+echo '<br>';
+
+$usuario2 = new Usuario();
+$usuario2->nombre='José';
+echo $usuario2->nombre;
+echo '<br>';
+echo $usuario2->saluda();
+
+echo "<br><br>{$usuario1->nombre} es amigo de {$usuario2->nombre}";
