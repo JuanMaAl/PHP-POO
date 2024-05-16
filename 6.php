17c1bbbf4269e3daf42ed25567f49307a94a4798
Juan Martínez Alonso <juanmaal1986@gmail.com>
2024-05-16 12:22:49 +0200

    Ejemplo de poliformismo, clases abstractas y métodos abstractos

---
+++ 6.php
@@ -0,0 +1,27 @@
+<?php
+    abstract class Usuario{
+            abstract public function asignarRol();
+    }
+    
+    class Administrador extends Usuario {
+        public function asignarRol(){
+            return 'Al usuario: '.__CLASS__.' le doy el rol de: ADMIN';
+        }
+    }
+
+    class Invitado extends Usuario {
+        public function asignarRol(){
+            return 'Al usuario: '.__CLASS__.' le doy el rol de: INVITADO';
+        }     
+    }
+    function verRol(Usuario $usuario){
+        echo $usuario->asignarRol();
+        echo '<br><br>';
+    }
+
+    //prueba de los objetos y las funciones
+    $usuarioAdmin = new Administrador();
+    $usuarioInvitado = new Invitado();
+
+    verRol($usuarioAdmin);
+    verRol($usuarioInvitado);
